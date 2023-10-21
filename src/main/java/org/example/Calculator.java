52049c3aac129c6e805cc1d3eae9ad6eb8ed4829
Sitthata <aprils96950@gmail.com>
2023-10-21 20:36:40 +0700

    finished calculator example

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,24 +7,18 @@ public class Calculator {
     public static void main(String[] args) {
         // Call your method here
         Scanner scanner = new Scanner(System.in);
-        System.out.println("choose first number");
+        System.out.print("Enter first number: ");
         int number1 = scanner.nextInt();
-        System.out.println("number first is " + number1);
-        // Enter second number
-        System.out.println("choose second number");
+        System.out.print("Enter second number: ");
         int number2 = scanner.nextInt();
-        System.out.println("number second is " + number2);
-        System.out.println("choose operation");
-        String operater = scanner.next();
+        System.out.print("choose operation (+, -, *, /): ");
+        String operator = scanner.next();
+        double result = calculate(number1, number2, operator);
+        System.out.printf("%d %s %d = %f", number1, operator, number2, result);
     }
 
-    // 10, 20, +
-    // calculate(10,20,+) 30
-    // calculate(20,10,-) 10
-    // calculate(5,0,/) -1
-
-    public static double calculate(int number1, int number2, String operater) {
-        switch (operater) {
+    public static double calculate(int number1, int number2, String operator) {
+        switch (operator) {
             case "+":
                 return number1 + number2;
             case "-":
@@ -35,11 +29,10 @@ public class Calculator {
                 if (number2 == 0) {
                     return -1;
                 } else {
-                    return (double)number1 / number2;
+                    return (double) number1 / number2;
                 }
-
             default:
-                return -2 ;
+                return -2;
         }
 
     }
