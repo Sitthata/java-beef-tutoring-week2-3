392bcf8a3c6f2d5491d793d463f27f49062f2ae3
Sitthata <aprils96950@gmail.com>
2023-10-21 21:00:34 +0700

    create warm up java

---
+++ src/main/java/org/example/PracticeArray.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class PracticeArray {
+    public static void main(String[] args) {
+        int[] array = {5, 3, 10, 24, 51, 14, 22, 67, 89, 91};
+        // Call your method here
+    }
+
+    // arraySum(int[] numbers)
+
+    // maxFinder(int[] numbers)
+
+    // average(int[] numbers)
+}
---
+++ src/main/java/org/example/WarmUp.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class WarmUp {
+    public static void main(String[] args) {
+
+    }
+
+    // Create a BMI calculator (weight, height). BMI = weight / height^2
+}
